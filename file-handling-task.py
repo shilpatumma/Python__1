17b3b7c908cd4ccdd521d9504d4af5ee8433ceab
shilpatumma <154594223+shilpatumma21@users.noreply.github.com>
2024-07-29 22:35:26 +0530

    second task completed

--- file-handling-task.py
+++ file-handling-task.py
@@ -5,16 +5,14 @@
 #     for line in f:
 #         words = line.split()
 #         num_of_words += len(words)
-# print("Number of words is : ")
-# print(num_of_words)
+# print("Number of words is :", num_of_words)
 
 
 # (2). search for a specific word in a file
-
-with open("file-handling-task.txt", "r") as f:
-    for line in f:
-        for word in line.split():
-            print(word)
-
-    
-
+with open("file-handling-task.txt",'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        if ("fox" in line):
+            print("Present")
+        else:
+            print("Not present")
