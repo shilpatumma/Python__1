691fa6dc5fa67978766d908de0e518e002a37669
shilpatumma <154594223+shilpatumma21@users.noreply.github.com>
2024-08-02 13:06:22 +0530

    change

--- file-handling-prac.py
+++ file-handling-prac.py
@@ -1,15 +1,15 @@
 # 1. Write a Python program to read an entire text file.
 # Ans...
 
-# f = open("file-handling-prac.txt", "r")
-# data = f.read()
-# print(data)
-# f.close()
+f = open("file-handling-prac.txt", "r")
+data = f.read()
+print(data)
+f.close()
 
 
 # (2). Write a Python program to read first n lines of a file.
 # Ans...
 
-f = open("file-handling-prac.txt", "r")
-print(f.readline())
-f.close()
+# f = open("file-handling-prac.txt", "r")
+# print(f.readline())
+# f.close()
