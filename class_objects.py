6bc78a2bd5103dc3eca4446beb4475868114926c
shilpatumma <154594223+shilpatumma21@users.noreply.github.com>
2024-08-07 23:07:17 +0530

    bank account created

--- class_objects.py
+++ class_objects.py
@@ -15,6 +15,57 @@
 
 
 
+# class Book:
+#     def __init__(self, title, pages, author, publication_date, price, copies_available, checked_out_by):
+#         self.t = title
+#         self.p = pages
+#         self.a = author
+#         self.pd = publication_date
+#         self.pr = price
+#         self.ca = copies_available
+#         self.cob = checked_out_by
+
+#     def checkout(self,name):
+#         self.cob.append(name)
+#         self.ca -= 1
+
+#     def check_in(self,name):
+#         self.cob.remove(name)
+#         self.ca += 1
+
+#     def add_copies(self,number):
+#         self.ca += number
+
+#     def remove_copies(self,number):
+#         self.ca -= number
+
+#     def display(self):
+#         print(self.t)
+#         print(self.p)
+#         print(self.a)        
+#         print(self.pd)
+#         print(self.pr)
+#         print(self.ca)
+#         print(self.cob)
+
+# Motivational_book = Book (
+#         title = "11 Rules For Life", 
+#         author = "Chetan Bhagat", 
+#         publication_date = "26 February 2024",
+#         pages = "256 pages",
+#         price = 171,
+#         copies_available = 5,
+#         checked_out_by = "Swastika"
+#     )
+
+# Motivational_book.display()
+# book1 = Book("11 Rules For Life", 256, "Chetan Bhagat", "26 February 2024", 171, 6, "Sneha")
+
+# book1.display()
+
+
+
+
 # class Employee:
 #     def __init__(self, name, salary):
 #         self.n = name
@@ -38,48 +89,72 @@
 
 
 
-class Book:
-    def __init__(self, title, pages, author, publication_date, price, copies_available, checked_out_by):
-        self.t = title
-        self.p = pages
-        self.a = author
-        self.pd = publication_date
-        self.pr = price
-        self.ca = copies_available
-        self.cob = checked_out_by
 
-    def checkout(self,name):
-        self.cob.append(name)
-        self.ca -= 1
+class Bank:
+    def __init__(self,account_number,name,initial_balance=0):
+        self.acc_num = account_number
+        self.n = name
+        self.balance = initial_balance
 
-    def check_in(self,name):
-        self.cob.remove(name)
-        self.ca += 1
+    def deposit(self,amount):
+        self.balance += amount
+        print(f"Deposited {amount}. New balance is : {self.balance}")
 
-    def add_copies(self,number):
-        self.ca += number
+    def withdraw(self,amount):
+        if(amount <= self.balance):
+            self.balance -= amount
+            print(f"Withdraw {amount}. New balance is : {self.balance}")
+        else:
+            print("Insufficient funds.")
 
+    def check_balance(self):
+        print(f"current balance is : {self.balance}")
 
-    def display(self):
-        print(self.t)
-        print(self.p)
-        print(self.a)        
-        print(self.pd)
-        print(self.pr)
-        print(self.ca)
-        print(self.cob)
+accounts = []
 
-Motivational_book = Book (
-        title = "11 Rules For Life", 
-        author = "Chetan Bhagat", 
-        publication_date = "26 February 2024",
-        pages = "256 pages",
-        price = 171,
-        copies_available = 5,
-        checked_out_by = "Swastika"
-    )
+while True:
+    print("\nBank Management System")
+    print("1. Create Account")
+    print("2. Deposit Amount")
+    print("3. Withdraw Amount")
+    print("4. Check Bank Balance")
+    print("5. Exit")
 
-Motivational_book.display()
-book1 = Book("11 Rules For Life", 256, "Chetan Bhagat", "26 February 2024", 171, 6, "Sneha")
+    choice = input("Enter your choice (1-5) : ")
 
-book1.display()
+    if (choice == "1"):
+        account_number = input("Enter your account number : ")
+        name = input("Enter Your name : ")
+        account = Bank(account_number,name)
+        accounts.append(account)
+        print("Congratulations your account created successfully...")
+
+    elif (choice == "2"):
+        account_number = input("Enter your account number : ")
+        if (account.acc_num == account_number):
+            amount = float(input("Enter deposit amount: "))
+            account.deposit(amount)
+        else:
+            print("Account Not Found.")
+
+    elif (choice == "3"):
+        account_number = input("Enter your account number : ")
+        if (account.acc_num == account_number):
+            amount = float(input("Enter deposit amount: "))
+            account.withdraw(amount)
+        else:
+            print("Account Not Found.")
+
+    elif (choice == "4"):
+        account_number = input("Enter your account number : ")
+        if (account.acc_num == account_number):
+            account.check_balance()
+        else:
+            print("Account Not Found.")
+
+    elif (choice == "5"):
+        print("Exiting the program...")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
